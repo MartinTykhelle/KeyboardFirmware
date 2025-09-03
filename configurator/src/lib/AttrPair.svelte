bbd6e107785fd3b27835e06d6d17d37d86a2aa1d
Martin Tykhelle <martintykhelle@gmail.com>
2025-09-03 13:40:07 +0200

    Update AttrPair.svelte

--- configurator/src/lib/AttrPair.svelte
+++ configurator/src/lib/AttrPair.svelte
@@ -2,7 +2,7 @@
   <span class="label"><slot name="label"></slot></span>
   <span class="content"><slot name="content"></slot></span>
 </div>
-
+<!--- This is a fucking table, fix it --->
 <style>
   div {
     display: flex;
